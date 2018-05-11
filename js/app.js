d23eb9870addb15b7d59c3345ebbabfde4bba1b1
vipulrawat <vipulrawat.imi@live.in>
2018-05-11 11:53:47 +0530

    Hardcoded

---
+++ js/app.js
@@ -0,0 +1,75 @@
+const json = { 
+  "results": [{ "gender": "male", "name": { "title": "mr", "first": "eugenio", "last": "ruiz" }, "location": { "street": "4957 calle de arganzuela", "city": "la palma", "state": "país vasco", "postcode": 97310 }, "email": "eugenio.ruiz@example.com", "login": { "username": "bigpeacock644", "password": "stryker", "salt": "7NwhemL2", "md5": "1dce961d8c762a288ea31ea8cff668e3", "sha1": "bd98a57f0222d870308c1a945ddb9560051d9e7e", "sha256": "8b45acd44ba942ce3d5e4b017b82fb4d7b91083b366f0902752984ac3f777ed5" }, "dob": "1980-06-19 12:45:30", "registered": "2007-12-22 14:20:36", "phone": "914-026-213", "cell": "670-308-314", "id": { "name": "DNI", "value": "45705704-Q" }, "picture": { "large": "https://randomuser.me/api/portraits/men/97.jpg", "medium": "https://randomuser.me/api/portraits/med/men/97.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/97.jpg" }, "nat": "ES" }, { "gender": "female", "name": { "title": "ms", "first": "cássia", "last": "gonçalves" }, "location": { "street": "5704 rua paraná ", "city": "itaboraí", "state": "rio grande do sul", "postcode": 35844 }, "email": "cássia.gonçalves@example.com", "login": { "username": "whitekoala921", "password": "remember", "salt": "zofwZXIf", "md5": "a3f193accb725a7b6562c11217160565", "sha1": "5e695733611e36589a7afd55acffc6e2745b8981", "sha256": "6444b8d63ef3ca10f9abe4246e2dcbcb17c8d2091480afc486ddf8757b9480c4" }, "dob": "1975-05-03 03:01:23", "registered": "2007-08-23 17:57:43", "phone": "(54) 7382-0212", "cell": "(83) 0307-8799", "id": { "name": "", "value": null }, "picture": { "large": "https://randomuser.me/api/portraits/women/21.jpg", "medium": "https://randomuser.me/api/portraits/med/women/21.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/21.jpg" }, "nat": "BR" }, { "gender": "male", "name": { "title": "mr", "first": "cândido", "last": "porto" }, "location": { "street": "8339 rua são josé ", "city": "dourados", "state": "bahia", "postcode": 97696 }, "email": "cândido.porto@example.com", "login": { "username": "whiteswan638", "password": "toaster", "salt": "G2urPa7l", "md5": "04f02dac8b0ff465668c073ee466309c", "sha1": "caa33c61fee30167bf21da7a0708939d84316976", "sha256": "d15ae01a51e30a93837d790d5f174662004fba15c2109b3355b02d15a97319bb" }, "dob": "1979-10-20 10:12:40", "registered": "2003-10-08 20:35:22", "phone": "(04) 9907-7824", "cell": "(56) 4827-6174", "id": { "name": "", "value": null }, "picture": { "large": "https://randomuser.me/api/portraits/men/76.jpg", "medium": "https://randomuser.me/api/portraits/med/men/76.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/76.jpg" }, "nat": "BR" }, { "gender": "female", "name": { "title": "mrs", "first": "olivia", "last": "olsen" }, "location": { "street": "1326 ryttervænget", "city": "sørvad", "state": "sjælland", "postcode": 73816 }, "email": "olivia.olsen@example.com", "login": { "username": "smallfrog997", "password": "midnight", "salt": "uJb7eaBH", "md5": "0fef383a4853114c5d064c73eef3a592", "sha1": "cdd3fcb572838d02a2ddcc03baf5450dbdbb6433", "sha256": "507e27278237bf5c465bb16e473a9a72c98d5f2540d3e7f9e2c868a9bbc0f1fd" }, "dob": "1950-05-02 20:17:09", "registered": "2004-03-31 22:36:40", "phone": "00552889", "cell": "73698945", "id": { "name": "CPR", "value": "644754-4948" }, "picture": { "large": "https://randomuser.me/api/portraits/women/16.jpg", "medium": "https://randomuser.me/api/portraits/med/women/16.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/16.jpg" }, "nat": "DK" }, { "gender": "female", "name": { "title": "ms", "first": "phoebe", "last": "king" }, "location": { "street": "1615 anzac parade", "city": "gisborne", "state": "marlborough", "postcode": 93771 }, "email": "phoebe.king@example.com", "login": { "username": "lazyswan191", "password": "maggot", "salt": "3SpwaInw", "md5": "d05cda15169da2683d684bc3646b9933", "sha1": "f6a0dbfc5b38d132cfcb59b7bf443e4ac3ccec16", "sha256": "3b03572dcf6d7db8868e3807246c40928af4a79d535c86d9a354b0ed019b4c9b" }, "dob": "1988-04-27 14:06:24", "registered": "2016-06-01 07:32:18", "phone": "(288)-923-6164", "cell": "(954)-347-3997", "id": { "name": "", "value": null }, "picture": { "large": "https://randomuser.me/api/portraits/women/37.jpg", "medium": "https://randomuser.me/api/portraits/med/women/37.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/37.jpg" }, "nat": "NZ" }, { "gender": "male", "name": { "title": "mr", "first": "alexander", "last": "grewal" }, "location": { "street": "4394 dufferin st", "city": "souris", "state": "northwest territories", "postcode": 72149 }, "email": "alexander.grewal@example.com", "login": { "username": "redmouse888", "password": "goats", "salt": "B3XMLi5l", "md5": "cbb595c31920ab9c2f3143b721c1ea00", "sha1": "43b325ec6315f49a5fa542078f366eb844185f53", "sha256": "ab034bfc35852eaabb3400b29006693b35a5e3d3dcd57548e8d52d81ceaef23e" }, "dob": "1947-12-23 17:24:18", "registered": "2004-04-26 04:26:28", "phone": "339-324-7140", "cell": "635-463-9626", "id": { "name": "", "value": null }, "picture": { "large": "https://randomuser.me/api/portraits/men/2.jpg", "medium": "https://randomuser.me/api/portraits/med/men/2.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/2.jpg" }, "nat": "CA" }, { "gender": "female", "name": { "title": "miss", "first": "frieda", "last": "stein" }, "location": { "street": "4772 gartenweg", "city": "schwandorf", "state": "bayern", "postcode": 94640 }, "email": "frieda.stein@example.com", "login": { "username": "bigkoala467", "password": "nocturne", "salt": "tcXOmjwq", "md5": "5f35a7bb91eaaa5cc3cb909e291cff7f", "sha1": "109326cc2b33d556856e714b840907d60d098b65", "sha256": "df654e6954234e8422f6c7cba4bf51c3fedb7fefa4a14e2b1d3b5b4302423ab7" }, "dob": "1959-08-17 06:35:23", "registered": "2015-04-13 05:37:48", "phone": "0800-9739385", "cell": "0173-1684851", "id": { "name": "", "value": null }, "picture": { "large": "https://randomuser.me/api/portraits/women/34.jpg", "medium": "https://randomuser.me/api/portraits/med/women/34.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/34.jpg" }, "nat": "DE" }, { "gender": "male", "name": { "title": "mr", "first": "samuel", "last": "white" }, "location": { "street": "3249 weymouth road", "city": "wellington", "state": "auckland", "postcode": 64132 }, "email": "samuel.white@example.com", "login": { "username": "purpleduck627", "password": "titans", "salt": "TOkokPCZ", "md5": "ffacbfb69cf1baa217636167cb4c3921", "sha1": "9970794c15b6f1ddbc22289dc177f352d0e0f924", "sha256": "cd1a9b26a5e76229e2946c19d75146f382ff775423219418ae77aae38a50e55c" }, "dob": "1994-08-07 01:35:54", "registered": "2004-06-19 15:54:53", "phone": "(880)-010-9347", "cell": "(953)-114-8820", "id": { "name": "", "value": null }, "picture": { "large": "https://randomuser.me/api/portraits/men/72.jpg", "medium": "https://randomuser.me/api/portraits/med/men/72.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/72.jpg" }, "nat": "NZ" }, { "gender": "male", "name": { "title": "mr", "first": "marco", "last": "martin" }, "location": { "street": "4580 calle de arturo soria", "city": "cuenca", "state": "castilla y león", "postcode": 30525 }, "email": "marco.martin@example.com", "login": { "username": "organicostrich902", "password": "secure", "salt": "qfSaixVF", "md5": "0338f37c1eee744331551bc54bb30f91", "sha1": "c00072682ed931bf700a90229a2d42a5346e3c3e", "sha256": "7ac73d66a99b8aaa4bf478ba994dad17dbdd5496fce6802e2dc39e7ac490647e" }, "dob": "1957-07-06 19:35:32", "registered": "2008-04-25 05:22:27", "phone": "995-960-503", "cell": "617-704-834", "id": { "name": "DNI", "value": "10053259-W" }, "picture": { "large": "https://randomuser.me/api/portraits/men/65.jpg", "medium": "https://randomuser.me/api/portraits/med/men/65.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/65.jpg" }, "nat": "ES" }, { "gender": "male", "name": { "title": "mr", "first": "lisandro", "last": "pires" }, "location": { "street": "8440 rua josé bonifácio ", "city": "piracicaba", "state": "alagoas", "postcode": 80763 }, "email": "lisandro.pires@example.com", "login": { "username": "whitefish195", "password": "audio", "salt": "kTc70Es2", "md5": "109aaff92c786e44105883d66082f5f4", "sha1": "57f01c2e26ee7536e84a3321f0537843a3e36c92", "sha256": "90e21da1f741c2020750b29b12fb9244ecc1fb314ee3a2abd20174f757ff6a3c" }, "dob": "1951-05-07 02:24:13", "registered": "2007-09-05 23:38:36", "phone": "(41) 9255-7297", "cell": "(33) 3379-6478", "id": { "name": "", "value": null }, "picture": { "large": "https://randomuser.me/api/portraits/men/62.jpg", "medium": "https://randomuser.me/api/portraits/med/men/62.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/62.jpg" }, "nat": "BR" }], "info": { "seed": "52739e0da38c7f2b", "results": 10, "page": 1, "version": "1.1" } }
+
+
+  
+const card = {
+  margin: '1rem',
+  background: 'white',
+  boxShadow: '2px 4px 25px rgba(0, 0, 0, .1)',
+  borderRadius: '12px',
+  overflow: 'hidden',
+  transition: 'all .2s linear'
+}
+const container = {
+  display: 'flex',
+  flexWrap: 'wrap'
+}
+const h1 = {
+ 
+  textAlign: 'center',
+  padding: '1.5rem 2.5rem',
+  backgroundImage: 'linear-gradient(120deg, #fbc2eb 0%, #a6c1ee 100%)',
+  margin: '0 0 2rem 0',
+  fontSize: '1.5rem',
+  color: 'white'
+
+}
+const img = {
+  display: 'block',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  width: '100px',
+  height: '100px',
+  borderRadius: '200px'
+}
+
+function FirstComponent({data}) {
+  return (
+    data.map(item => 
+            (
+              <div key={item.cell} style={card}>
+                <h1 style={h1}>{item.name}</h1>
+                <img src={item.picture} style={img}></img>
+                <p><b>Email:</b>{item.email}</p>
+                <p><b>Cell:</b>{item.cell} <b>Gender:</b>{item.gender}</p>
+              </div>
+            ))
+  );
+}
+
+class App extends React.Component {
+
+  render() {
+    var arr = [];
+    json.results.forEach(data => {
+      arr.push({
+        name: data.name.first+' '+data.name.last,
+        gender: data.gender,
+        email: data.email,
+        cell: data.cell,
+        picture: data.picture.large
+      })
+    })
+    console.log(arr);
+      return(
+        <div style={container}>
+          <FirstComponent data={arr}/>
+          </div>
+      );
+ 
+   
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
